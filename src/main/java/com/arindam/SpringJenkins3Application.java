5c24c2c37b69a7abb7b92865ecc234b936b1f885
arindamslt <arin.saltlake@gmail.com>
2022-11-13 21:45:09 +0530

    first commit

---
+++ src/main/java/com/arindam/SpringJenkins3Application.java
@@ -0,0 +1,13 @@
+package com.arindam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkins3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkins3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arindam/SpringJenkins3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arindam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkins3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
